61ad74cbccbdc0e8eedac2cf2859feba73eca4da
arifinfrds <arifinfrds.engineer@gmail.com>
2022-06-06 19:08:32 +0700

    Initial Commit

---
+++ DownloadFileDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DownloadFileDemo
+//
+//  Created by Arifin Firdaus on 06/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DownloadFileDemo/DownloadFileDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadFileDemoApp.swift
+//  DownloadFileDemo
+//
+//  Created by Arifin Firdaus on 06/06/22.
+//
+
+import SwiftUI
+
+@main
+struct DownloadFileDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
